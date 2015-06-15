e3d513ab10a6f0c3d3a52d53fad3f5e00d2d8241
Jochen Issing <iss@nesono.com>
2015-06-15 23:17:23 +0200

    fix scales

--- report.R
+++ report.R
@@ -6,7 +6,7 @@ if(!require(gridExtra)){
   install.packages('gridExtra')
   require(gridExtra)
 }
-
+library(scales)
 source('helpers.R')
 
 datafile <- 'kontotransactionlist.csv'
@@ -15,14 +15,14 @@ data <- read.delim( datafile )
 data <- sanetizedata(data)
 
 
-g.balance <- ggplot( data=data, aes(x=Date, y=Balance/1000), color="green")
-g.balance <- g.balance + ylab("Balance k SEK") + xlab("Transaction Date")
-g.balance <- g.balance + geom_line(colour = 'green3') + geom_smooth(colour='black')
+g.balance <- ggplot( data=data, aes(x=Date, y=Balance), color="green")
+g.balance <- g.balance + ylab("Balance SEK") + xlab("Transaction Date")
+g.balance <- g.balance + geom_line(colour = 'green3') + geom_smooth(colour='black') + scale_y_continuous(labels=comma)
 
 col.sign <- c('red3','green3')
 col.bar <- col.sign[(sign(data$Amount)+1)/2+1]
-g.volume <- ggplot( data=data, aes(Date, abs(Amount/1000))) + ylab("Volume")
-g.volume <- g.volume + ylab("Volume k SEK") + xlab("Transaction Date")
-g.volume <- g.volume + geom_bar(colour=col.bar, stat = "identity")
+g.volume <- ggplot( data=data, aes(Date, abs(Amount))) + ylab("Volume")
+g.volume <- g.volume + ylab("Volume SEK") + xlab("Transaction Date")
+g.volume <- g.volume + geom_bar(colour=col.bar, stat = "identity") + scale_y_continuous(labels=comma)
 
 grid.arrange(g.balance, g.volume, nrow=2)
